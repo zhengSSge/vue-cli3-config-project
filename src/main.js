6ba341e81f70c3a4a8f572d7ed538b9e901a4a4e
zhengzhen <827807932@qq.com>
2019-10-09 18:00:04 +0800

    第三次提交

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import {baseUrl} from '../env'
 
 Vue.config.productionTip = false;
-
+console.log(baseUrl);
 new Vue({
   router,
   store,
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path =  require('path'); //引入path模块（node）
+const resolve = (dir) => path.join(__dirname, dir); //将文件组成绝对路径
+
+module.exports = {
+  //部署应用包时的基本 URL
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+  //当运行 vue-cli-service build 时生成的生产环境构建文件的目录
+  outputDir: 'distis',
+  //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: 'assets',
+  // eslint-loader 是否在保存的时候检查 安装@vue/cli-plugin-eslint有效 false 关闭eslint
+  lintOnSave: false,
+  //是否使用包含运行时编译器的 Vue 构建版本。设置true后你就可以在使用template
+  runtimeCompiler: true,
+  // 生产环境是否生成 sourceMap 文件 sourceMap的详解请看末尾
+  productionSourceMap: false,
+  chainWebpack: config => {
+    // 添加别名
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'))
+  }
+};
